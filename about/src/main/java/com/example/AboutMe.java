4181fa4937a5006442b3c1675f7e2c773243e791
Meri Winchester <mwinchester@mchenry.edu>
2017-02-27 19:20:33 -0600

    Initial commit

---
+++ about/src/main/java/com/example/AboutMe.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class AboutMe {
+
+    public static void main(String[] args){
+        String myName = "Meri WInchester";
+        String major = "Martial Arts";
+        double gpa = 4.0;
+        char grade = 'A';
+        int started = 2001;
+        System.out.println("Hello, my name is " + myName);
+        System.out.println("I am majoring in " + major);
+        System.out.println("My goal this semester is to gt a " + gpa + " or higher for my GPA");
+        System.out.println("I hope to get an " + grade + " in this class");
+        System.out.println("I first started at MCC in " + started);
+    }
+
+}
---
+++ variables/src/main/java/com/example/InitializeVariables.java
@@ -0,0 +1,35 @@
+package com.example;
+/* Author : Meri Winchester
+   Assignment: Language Fundamentals - Data Types
+ */
+
+public class InitializeVariables {
+    public static void main(String[] args){
+        byte myByte = 2;
+        short myShort = 22;
+        int myInt = 1;
+        long myLong = 1343234;
+        float myFloat = 1.0f;
+        double myDouble = 3.14;
+        boolean myBool = true;
+        char myChar = 'A';
+
+        // printing the variables
+
+        System.out.println("myByte: " + myByte);
+        System.out.println("myShort: " + myShort);
+        System.out.println("myInt: " + myInt);
+        System.out.println("myLong: " + myLong);
+        System.out.println("myFloat: " + myFloat);
+        System.out.println("myDouble: " + myDouble);
+        System.out.println("myBool: " + myBool);
+        System.out.println("myChar: " + myChar);
+
+
+
+
+
+
+
+    }
+}
